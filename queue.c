6565687badd13711f49e61faa03e928108c9872c
Chi-Kuan <devaraja1945@gmail.com>
2025-03-09 03:17:03 +0800

    Implement naive approach for deleting duplicates
    Implement a naive approach to remove duplicate nodes from the linked
    list Use the 'list_for_each_safe' macro to traverse the list. If the
    current node shares the same value as the next node, repeatedly delete
    the next node in a while loop until no duplicates remain.
    
    Change-Id: Id98b58634d743f61bf8ffc6aa783ea255d052274

--- queue.c
+++ queue.c
@@ -158,7 +158,24 @@ bool q_delete_mid(
 /* Delete all nodes that have duplicate string */
 bool q_delete_dup(struct list_head *head)
 {
-    // https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/
+    if (!head)
+        return false;
+    element_t *entry;
+    element_t *safe;
+    list_for_each_entry_safe(entry, safe, head, list) {
+        while (entry->list.next != head &&
+               !strcmp(entry->value,
+                       list_entry(entry->list.next, element_t, list)->value)) {
+            element_t *Next = list_entry(entry->list.next, element_t, list);
+            list_del(&Next->list);
+            q_release_element(Next);
+        }
+        if (list_entry(entry->list.next, element_t, list) != safe) {
+            safe = list_entry(entry->list.next, element_t, list);
+            list_del_init(&entry->list);
+            q_release_element(entry);
+        }
+    }
     return true;
 }
 
